b563adcd3216cc79d19acdca84bba0de28c527b9
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-05-12 09:50:04 +0530

    add: code for loop size in a linked list

--- data-structures/linked-list/problems/loop-size.cpp
+++ data-structures/linked-list/problems/loop-size.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "../generic-node.h"
 
 using namespace std;
@@ -16,8 +17,47 @@ void push(Node<int>** head, int key) {
   *head = temp;
 }
 
-int main() {
-	
-	return 0; 
+int loopSize(Node<int>* head) {
+  auto rabbit = head;
+  auto hare = head;
+
+  while (hare && rabbit && rabbit->next) {
+    
+		rabbit = rabbit->next->next;
+    hare = hare->next;
+
+		// loop detected
+		if (rabbit == hare) {
+			// count the size of the loop
+			int count = 1;
+			while (hare->next != rabbit) {
+				count++;
+				hare = hare->next;
+			}
+			return count;
+		}
+  }
+
+  return -1;
 }
 
+int main() {
+  auto head = new Node<int>(1);
+
+  auto second = new Node<int>(2);
+  auto third = new Node<int>(3);
+  auto fourth = new Node<int>(4);
+  auto fifth = new Node<int>(5);
+
+  head->next = second;
+  second->next = third;
+  third->next = fourth;
+  fourth->next = fifth;
+  fifth->next = third;
+
+  auto size = loopSize(head);
+
+  cout << "loop size is " << size << endl;
+
+  return 0;
+}
