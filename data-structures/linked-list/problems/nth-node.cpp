682d7a72edef07398fc224c8dbe85b64bcfd9f98
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-05-13 19:40:10 +0530

    add: logic for nth node

--- data-structures/linked-list/problems/nth-node.cpp
+++ data-structures/linked-list/problems/nth-node.cpp
@@ -17,7 +17,39 @@ void push(Node<int>** head, int key) {
   *head = temp;
 }
 
+Node<int>* getNthNode(Node<int>* head, int n) {
+	int count = 0;
+
+	while (head) {
+		if (count == n) {
+			return head;
+		}
+		head = head ->next;
+		count++;
+	}
+
+	return nullptr;
+}
+
 int main() {
-	
+	Node<int>* head = nullptr;
+
+	push(&head, 1);
+	push(&head, 2);
+	push(&head, 3);
+	push(&head, 4);
+	push(&head, 5);
+	push(&head, 6);
+
+	printList(head);
+
+	auto thirdNode = getNthNode(head, 8);
+
+	if (thirdNode) {
+		cout << "Eight node of the above linked list is " << thirdNode->data << endl;
+	} else {
+		cout << "Requested node could not be sent" << endl;
+	}
+
 	return 0;
 }
