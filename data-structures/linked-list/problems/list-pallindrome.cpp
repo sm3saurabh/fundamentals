3f47e1f0d4d0b733d14362587c317153f4cca29b
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-05-14 21:07:58 +0530

    start list pallindrome

---
+++ data-structures/linked-list/problems/list-pallindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+#include "../generic-node.h"
+
+using namespace std;
+
+void printList(Node<int>* head) {
+  while (head) {
+    cout << head->data << endl;
+    head = head->next;
+  }
+}
+
+void push(Node<int>** head, int key) {
+  auto temp = new Node(key);
+  temp->next = *head;
+  *head = temp;
+}
+
+
+
+
+
