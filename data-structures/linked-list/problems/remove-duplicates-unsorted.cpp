c19696bc83321c59c4012ee433b5ecbcc5c0b77f
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-05-15 20:29:33 +0530

    add: logic to remove duplicates in an unsorted linked list using nested loops

--- data-structures/linked-list/problems/remove-duplicates-unsorted.cpp
+++ data-structures/linked-list/problems/remove-duplicates-unsorted.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "../generic-node.h"
 
 using namespace std;
@@ -16,3 +17,59 @@ void push(Node<int>** head, int key) {
   *head = temp;
 }
 
+void removeDuplicatesUnsorted(Node<int>* head) {
+  Node<int>* outer = head;
+
+	while (outer != nullptr) {
+		Node<int>* current = head;
+		Node<int>* prev = nullptr;
+
+		while (current != nullptr) {
+			if (current == outer) {
+				prev = current;
+				current = current->next;
+				continue;
+			}
+
+			if (current->data == outer->data) {
+				Node<int>* next = current->next;
+
+				delete current;
+
+				current = next;
+				prev->next = current;
+
+			} else {
+				prev = current;
+				current = current->next;
+			}
+		}
+
+		outer = outer->next;
+ 	} 
+  
+}
+
+int main() {
+  Node<int>* head = nullptr;
+
+  cout << endl << "Testing remove duplicates" << endl;
+
+  push(&head, 23);
+  push(&head, 42);
+  push(&head, 11);
+  push(&head, 23);
+  push(&head, 37);
+  push(&head, 42);
+  push(&head, 11);
+
+  printList(head);
+
+  cout << endl;
+
+  removeDuplicatesUnsorted(head);
+
+  cout << "Remove duplicates has been called" << endl;
+
+  printList(head);
+}
