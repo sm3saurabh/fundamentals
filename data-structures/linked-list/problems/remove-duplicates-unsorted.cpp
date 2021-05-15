05d817783adb26f468778a2a075ca6ac160f17cb
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-05-15 19:39:04 +0530

    add: remove duplicates from an unsorted linked list

---
+++ data-structures/linked-list/problems/remove-duplicates-unsorted.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../generic-node.h"
+
+using namespace std;
+
+void printList(Node<int>* head) {
+  while (head) {
+    cout << head->data << endl;
+    head = head->next;
+  }
+}
+
+void push(Node<int>** head, int key) {
+  auto temp = new Node(key);
+  temp->next = *head;
+  *head = temp;
+}
+
